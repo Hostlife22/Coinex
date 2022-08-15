bc42c6a34e776a13c327d1a406a4e06251d98994
Serafim Sen <sima.serafim2021@mail.ru>
2022-08-15 09:35:58 +0300

    feat: connected redax

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import { store } from './app/store';
 import './index.css';
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools();
 }
+console.log('Production mode', process.env.NODE_ENV === 'production');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
