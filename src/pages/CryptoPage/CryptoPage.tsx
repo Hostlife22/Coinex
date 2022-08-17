e8365acfef509c3607b4e81275386f3e8b8b9d72
Serafim Sen <sima.serafim2021@mail.ru>
2022-08-17 15:26:23 +0300

    feat: added button

--- src/pages/CryptoPage/CryptoPage.tsx
+++ src/pages/CryptoPage/CryptoPage.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import { useMemo, useState } from 'react';
+import { FaPlus } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import { days } from '../../common/data';
 import { Button, Card, Chart, CryptoDetails, Htag, Ptag } from '../../components';
@@ -10,7 +11,7 @@ import './CryptoPage.scss';
 const CryptoPage = () => {
   const { id } = useParams();
   const [selectedDay, setSelectedDay] = useState<number>(7);
-  const { data: cryptosData } = useGetCryptoHistoryQuery({ id: id || '', interval: 'd1' });
+  const { data: cryptosData, isLoading } = useGetCryptoHistoryQuery({ id: id || '', interval: 'd1' });
   const { data: coinData } = useGetCryptoQuery(id || '');
 
   const currentData = useMemo(() => {
@@ -24,23 +25,30 @@ const CryptoPage = () => {
   return (
     <div className="crypto">
       <Card className="crypto__container">
-        <Htag tag="h1" className="crypto__title">
-          {coinData?.data.name}{' '}
-        </Htag>
+        <div className="crypto__wrapper">
+          <Htag tag="h1" className="crypto__title">
+            {coinData?.data.name}{' '}
+          </Htag>
+          <Button appearance="primary" className="crypto__add-portfolio">
+            <FaPlus /> ADD
+          </Button>
+        </div>
+
         <Ptag className="crypto__subtitle">
           {coinData?.data.name} live price in USD dollars. View value statistics, market cap and supply.
         </Ptag>
         <Chart data={currentData} />
         <div className="crypto__btns">
-          {days.map((day) => (
-            <Button
-              appearance="primary"
-              className={cn('crypto__btn', { crypto__btn_active: selectedDay === day.day })}
-              key={day.id}
-              onClick={() => setSelectedDay(day.day)}>
-              {day.day}d
-            </Button>
-          ))}
+          {!isLoading &&
+            days.map((day) => (
+              <Button
+                appearance="primary"
+                className={cn('crypto__btn', { crypto__btn_active: selectedDay === day.day })}
+                key={day.id}
+                onClick={() => setSelectedDay(day.day)}>
+                {day.day}d
+              </Button>
+            ))}
         </div>
 
         {coinData?.data && <CryptoDetails {...coinData.data} />}
