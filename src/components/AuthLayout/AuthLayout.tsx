ca4a1ef0ecd5cedbb0540eefbe49c071b7a76c17
Serafim Sen <sima.serafim2021@mail.ru>
2022-08-19 19:35:38 +0300

    feat: add button goback

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -1,16 +1,22 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { useAuth } from '../../hooks/useAuth';
+import Button from '../Button/Button';
 import './AuthLayout.scss';
 
 const AuthLayout = () => {
   const { auth } = useAuth();
+  const navigate = useNavigate();
 
   if (auth) {
     return <Navigate to={'/'} replace={true} />;
   }
   return (
     <main className="container">
+      <Button appearance="primary" className="home-btn" onClick={() => navigate('/')}>
+        <FaHome title="Go home" />
+      </Button>
       <Outlet />
       <ToastContainer autoClose={2000} />
     </main>
