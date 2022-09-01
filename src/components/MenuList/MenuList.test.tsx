c54dadc92ee2c51fbac22019366cc4e35617bac1
hostlife22 <sima.serafim2021@mail.ru>
2022-09-01 12:21:31 +0300

    feat: wrote a test for the MenuList component

---
+++ src/components/MenuList/MenuList.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import { ElemProvider } from '../../common/utils/testUtils';
+import MenuList from './MenuList';
+
+describe('MenuList component', () => {
+  it('renders correctly', () => {
+    render(
+      <ElemProvider route="/">
+        <MenuList />
+      </ElemProvider>,
+    );
+
+    expect(screen.getByText(/dashboard/i)).toBeInTheDocument();
+    expect(screen.getByText(/revenue/i)).toBeInTheDocument();
+    expect(screen.getByText(/wallet/i)).toBeInTheDocument();
+  });
+
+  it('renders correctly with custom className', () => {
+    render(
+      <ElemProvider route="/">
+        <MenuList />
+      </ElemProvider>,
+    );
+
+    const activeLink = screen.getByRole('link', { current: 'page' });
+
+    expect(activeLink).toBeInTheDocument();
+    expect(activeLink).toHaveTextContent(/dashboard/i);
+  });
+});
